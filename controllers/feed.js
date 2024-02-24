050cb4b3c41a2df105464a10e6f579aa282c2398
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-02-25 01:14:42 +0200

    wip - post model and crud operations

---
+++ controllers/feed.js
@@ -0,0 +1,37 @@
+const { validationResult } = require('express-validator/check');
+
+exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{
+            title: 'First Post',
+            content: 'This is the first post!',
+            image: 'images/duck.jpeg',
+            creator: {
+                name: 'Masi'
+            },
+            createdAt: new Date()
+        }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    };
+    const title = req.body.title;
+    const content = req.body.content;
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: { name: 'Masi' },
+            createdAt: new Date()
+        }
+    });
+}
