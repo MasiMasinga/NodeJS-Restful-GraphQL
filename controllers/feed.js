eb15c97fac78aaa965c50373766c965c34945016
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-02-25 18:58:18 +0200

    feat - add pagination for posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,10 +3,21 @@ const path = require('path');
 const fs = require('fs');
 const Post = require('../models/post');
 
-exports.getPosts = (_req, res, next) => {
+exports.getPosts = (req, res, next) => {
 
-    Post.find().then(posts => {
-        res.status(200).json({ message: 'Fetched posts successfully.', posts: posts });
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+
+    Post.find().countDocuments().then(count => {
+        totalItems = count;
+        return Post.find().skip((currentPage - 1) * perPage).limit(perPage);
+    }).then(posts => {
+        res.status(200).json({
+            message: 'Fetched posts successfully.',
+            posts: posts,
+            totalItems: totalItems
+        });
     }).catch(err => {
         if (!err.statusCode) {
             err.statusCode = 500;
